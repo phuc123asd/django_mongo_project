5ca06fd4864e7217b4f693c968f3a882889acc14
phuc123asd <vphuc6899@gmail.com>
2025-11-14 03:45:25 +0700

    dd

--- api/urls/review.py
+++ api/urls/review.py
@@ -6,5 +6,4 @@ from api.views.review import *
 urlpatterns = [    
     path('get_by_id/<str:product_id>/', get_reviews_by_product_id, name='get_reviews_by_product_id'),
     path('add/', add_review_insecure, name='add_review_insecure'),
-    path('<str:review_id>/delete/', delete_review, name='delete_review'), 
 ]
