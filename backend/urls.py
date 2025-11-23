231012c4b2cc8e2f62a463385235b9cfe676c39a
phuc123asd <vphuc6899@gmail.com>
2025-11-23 15:21:04 +0700

    dd

--- backend/urls.py
+++ backend/urls.py
@@ -1,8 +1,13 @@
 # backend/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home_view(request):
+    return HttpResponse("Welcome to Django Mongo Project API!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_view, name='home'),
     path('api/', include('api.urls')),
 ]
